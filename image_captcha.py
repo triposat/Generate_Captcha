dcba46e0180d268b796589d23bc10775641f81bc
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-08-01 14:15:14 +0530

    Create image_captcha.py

---
+++ image_captcha.py
@@ -0,0 +1,14 @@
+# Import the following modules
+from captcha.image import ImageCaptcha
+  
+# Create an image instance of the gicen size
+image = ImageCaptcha(width = 280, height = 90)
+  
+# Image captcha text
+captcha_text = 'GeeksforGeeks'  
+  
+# generate the image of the given text
+data = image.generate(captcha_text)  
+  
+# write the image on the given file and save it
+image.write(captcha_text, 'CAPTCHA.png')
